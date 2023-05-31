2b3fbfa7338796313b1d016c8328003aa48c1359
paranoidFrappe <delectable999@gmail.com>
2023-05-31 08:01:54 -0700

    drag and drop 0800

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+const app = express();
+const server = require("http").createServer(app);
+// const http = require('http');
+// const socketIO = require('socket.io');
+const io = require('socket.io')(server);
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-const app = express();
-const server = http.Server(app);
-const io = socketIO(server, {
-  pingTimeout: 1000, // how many ms without a pong packet to consider the connection closed
-  pingInterval: 3000, // how many ms before sending a new ping packet
-});
+const { disconnect } = require("process");
+
+// const server = http.Server(app);
+// const io = socketIO(server);
+// const io = socketIO(server, {
+//   pingTimeout: 1000, // how many ms without a pong packet to consider the connection closed
+//   pingInterval: 3000, // how many ms before sending a new ping packet
+// });
 
 // temp storage to store tasks
 let storage = [[], [], [], []];
@@ -135,7 +140,13 @@ const generateUniqueAnonName = () => {
 // Serve static files in the /dist folder
 app.use('/', express.static(path.join(__dirname, '../dist')));
 app.get('/', (req, res) => res.sendFile(__dirname, '../dist/index.html'));
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//create a card deck for folllowing up the status
+const CardCollections = {};
 
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 // SocketIO listeners
 // socket refers to the client
 // io refers this server
@@ -178,21 +189,16 @@ io.on('connection', (socket) => {
   });
 
   // Listener for the 'greeting-from-client'
-  socket.on('add-task', (content) => {
+  socket.on('add-card', (content) => {
     // Assign a unique id for the task
     const uuid = uuidv4();
-
-    //store it to the first index of storage (TO DO column)
-    storage[0].push({
+    let newCard = {
       author: anonName,
       content,
       uuid: uuid,
-    });
-    io.emit('add-task', {
-      author: anonName,
-      content,
-      uuid: uuid,
-    });
+    }
+    CardCollections[newCard.uuid] = newCard;
+    io.emit('add-card-Return', newCard);
   });
 
   //Listener for 'delete-message'
@@ -205,74 +211,74 @@ io.on('connection', (socket) => {
   });
 
   //Listener for 'next'
-  socket.on('move-task-right', (uuid) => {
-    let foundTask = null;
-    let foundColumnIndex;
-    // find the task with the matching UUID and its current column index
-    for (let i = 0; i < storage.length; i++) {
-      // store current column
-      const column = storage[i];
-      // store index if uuid is found
-      const taskIndex = column.findIndex((task) => task.uuid === uuid);
+  // socket.on('move-task-right', (uuid) => {
+  //   let foundTask = null;
+  //   let foundColumnIndex;
+  //   // find the task with the matching UUID and its current column index
+  //   for (let i = 0; i < storage.length; i++) {
+  //     // store current column
+  //     const column = storage[i];
+  //     // store index if uuid is found
+  //     const taskIndex = column.findIndex((task) => task.uuid === uuid);
 
-      // if match was found and in the 2nd to last column (COMPLETE)...
-      if (taskIndex !== -1 && i === storage.length - 2) {
-        // remove the task at the specified index from the column array
-        foundTask = column.splice(taskIndex, 1)[0];
-        // create a current reviewer in storage
-        foundTask.reviewedBy = anonNamesObj[socket.id];
-        foundColumnIndex = i;
-        break;
-      }
-      // if match was found and not in the last column...
-      else if (taskIndex !== -1 && i !== storage.length - 1) {
-        // remove the task at the specified index from the column array
-        foundTask = column.splice(taskIndex, 1)[0];
-        foundColumnIndex = i;
-        break;
-      }
-    }
-    if (foundTask !== null) {
-      // push foundTask into next column in storage
-      storage[foundColumnIndex + 1].push(foundTask);
-    }
-    io.emit('move-task-right', { uuid, reviewerId: socket.id });
-  });
+  //     // if match was found and in the 2nd to last column (COMPLETE)...
+  //     if (taskIndex !== -1 && i === storage.length - 2) {
+  //       // remove the task at the specified index from the column array
+  //       foundTask = column.splice(taskIndex, 1)[0];
+  //       // create a current reviewer in storage
+  //       foundTask.reviewedBy = anonNamesObj[socket.id];
+  //       foundColumnIndex = i;
+  //       break;
+  //     }
+  //     // if match was found and not in the last column...
+  //     else if (taskIndex !== -1 && i !== storage.length - 1) {
+  //       // remove the task at the specified index from the column array
+  //       foundTask = column.splice(taskIndex, 1)[0];
+  //       foundColumnIndex = i;
+  //       break;
+  //     }
+  //   }
+  //   if (foundTask !== null) {
+  //     // push foundTask into next column in storage
+  //     storage[foundColumnIndex + 1].push(foundTask);
+  //   }
+  //   io.emit('move-task-right', { uuid, reviewerId: socket.id });
+  // });
 
   //Listener for 'previous'
-  socket.on('move-task-left', (uuid) => {
-    let foundTask = null;
-    let foundColumnIndex;
-    // find the task with the matching UUID and its current column index
-    for (let i = 0; i < storage.length; i++) {
-      // store current column
-      const column = storage[i];
-      // store index if uuid is found
-      const taskIndex = column.findIndex((task) => task.uuid === uuid);
+//   socket.on('move-task-left', (uuid) => {
+//     let foundTask = null;
+//     let foundColumnIndex;
+//     // find the task with the matching UUID and its current column index
+//     for (let i = 0; i < storage.length; i++) {
+//       // store current column
+//       const column = storage[i];
+//       // store index if uuid is found
+//       const taskIndex = column.findIndex((task) => task.uuid === uuid);
 
-      // if match was found and in the last column (REVIEWED)...
-      if (taskIndex !== -1 && i == storage.length - 1) {
-        // remove the task at the specified index from the column array
-        foundTask = column.splice(taskIndex, 1)[0];
-        // delete the reviewer
-        delete foundTask.reviewedBy;
-        foundColumnIndex = i;
-        break;
-      }
-      // if match was found and not in the first column...store result and column index
-      else if (taskIndex !== -1 && i !== 0) {
-        // remove the task at the specified index from the column array
-        foundTask = column.splice(taskIndex, 1)[0];
-        foundColumnIndex = i;
-        break;
-      }
-    }
-    if (foundTask !== null) {
-      // push foundTask into previous column in storage
-      storage[foundColumnIndex - 1].push(foundTask);
-    }
-    io.emit('move-task-left', uuid);
-  });
+//       // if match was found and in the last column (REVIEWED)...
+//       if (taskIndex !== -1 && i == storage.length - 1) {
+//         // remove the task at the specified index from the column array
+//         foundTask = column.splice(taskIndex, 1)[0];
+//         // delete the reviewer
+//         delete foundTask.reviewedBy;
+//         foundColumnIndex = i;
+//         break;
+//       }
+//       // if match was found and not in the first column...store result and column index
+//       else if (taskIndex !== -1 && i !== 0) {
+//         // remove the task at the specified index from the column array
+//         foundTask = column.splice(taskIndex, 1)[0];
+//         foundColumnIndex = i;
+//         break;
+//       }
+//     }
+//     if (foundTask !== null) {
+//       // push foundTask into previous column in storage
+//       storage[foundColumnIndex - 1].push(foundTask);
+//     }
+//     io.emit('move-task-left', uuid);
+//   });
 });
 
 server.listen(3000, () => console.log('The server is running at port 3000'));
